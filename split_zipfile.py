af98cc47a335610ca1754ff195645a57f4839653
Seyed Mohammad Amin Ghasemi سید محمد امین قاسمی <42154749+s-m-amin-ghasemi@users.noreply.github.com>
2019-11-01 20:32:21 +0330

    Create split_zipfile.py

---
+++ split_zipfile.py
@@ -0,0 +1,13 @@
+outfile = archive_name
+packet_size = int(1.5 * 1024**3)   # bytes
+
+with open(outfile, "rb") as output:
+    filecount = 0
+    while True:
+        data = output.read(packet_size)
+        print(len(data))
+        if not data:
+            break   # we're done
+        with open("{}{:03}".format(outfile, filecount), "wb") as packet:
+            packet.write(data)
+        filecount += 1
