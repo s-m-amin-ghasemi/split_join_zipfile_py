7885e30107e5b3439243a9459c1d2c2ce0d298e6
Seyed Mohammad Amin Ghasemi سید محمد امین قاسمی <42154749+s-m-amin-ghasemi@users.noreply.github.com>
2019-11-01 21:04:05 +0330

    Update join_zipfile.py

--- join_zipfile.py
+++ join_zipfile.py
@@ -1,9 +1,10 @@
-outfile = archive_name   #your zip file name  "--------.zip"
-packet_size = int(1024*1024*100)   # bytes
-filenumbers=9   #number of files you want to join
-for i in range(filenumbers):
-    with open("{}.zip{:03}".format(outfile, i), "rb") as packet:
-        col=packet.read(packet_size)
+def join_zipfile(path=./,fn=archive_name.zip,package_size_megabyte=100,filenumbers)
+    outfile = filename   #your zip file name  "--------.zip"
+        packet_size = int(1024*1024*package_size_megabyte)   # bytes
+    filenumbers=fn   #number of files you want to join
+    for i in range(filenumbers+1):
+        with open("{}{}.zip{:03}".format(path,outfile, i), "rb") as packet:
+            col=packet.read(packet_size)
 
-        with open("{}02.zip".format(outfile), "ab+") as mainpackage:
-            mainpackage.write(col)
+            with open("{}{}02.zip".format(path,outfile), "ab+") as mainpackage:
+                mainpackage.write(col)
