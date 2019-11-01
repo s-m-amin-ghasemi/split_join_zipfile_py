cb1deeae08d02a63916f85994a94950d7755c998
Seyed Mohammad Amin Ghasemi سید محمد امین قاسمی <42154749+s-m-amin-ghasemi@users.noreply.github.com>
2019-11-01 21:37:20 +0330

    Update split_zipfile.py

--- split_zipfile.py
+++ split_zipfile.py
@@ -1,5 +1,5 @@
-def split_zipfile(fn="archive_name.zip",package_size_megabyte=100,path=./)
-    inputfile = filename   #your zip file name  "--------.zip"
+def split_zipfile(fn="archive_name.zip",package_size_megabyte=100,path="./"):
+    inputfile = fn   #your zip file name  "--------.zip"
     packet_size = int(1024*1024*package_size_megabyte)   # bytes
 
     with open(path+inputfile, "rb") as output:
